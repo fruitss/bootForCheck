f720797c668dc2be5503a742b9f7810c135d1ca1
liushiyu <liushiyu@agilestar.cn>
2024-10-31 13:54:03 +0800

    first commit

---
+++ src/main/java/com/example/bootforcheck/BootForCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootforcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootForCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootForCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootforcheck/BootForCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootforcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootForCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
